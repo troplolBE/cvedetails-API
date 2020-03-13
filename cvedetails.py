901ad5f49faf95653d7942b73b2022118a942105
troplolBE <tcastron@student.s19.be>
2020-03-13 11:11:50 -0400

    updated script and requirements

--- cvedetails.py
+++ cvedetails.py
@@ -4,7 +4,6 @@ import urllib2
 import csv
 import re
 import argparse
-import pandas
 
 url = 'http://www.cvedetails.com/vulnerability-list/vendor_id-{vid}/product_id-{pid}/version_id-{verid}/'
 
@@ -15,9 +14,9 @@ hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML,
     'Accept-Language': 'en-US,en;q=0.8',
     'Connection': 'keep-alive'}
 
-headers = ["#", "CVE ID", "CWE ID", "# of Exploits", "Type", "Publish Date", "Update Date", "Score", "Gained Access", "Access", "Complexity", "Authentication", "Conf.", "Integ.", "Avail.", "version-id", "Description"]
+headers = ["#", "CVE ID", "CWE ID", "# of Exploits", "Type", "Publish Date", "Update Date", "Score", "Gained Access", "Access", "Complexity", "Authentication", "Conf.", "Integ.", "Avail.", "Description"]
 
-def get_cves_from_page(url, filename, version):
+def get_cves_from_page(url, filename):
     req = urllib2.Request(url, None, hdr)
     http = urllib2.urlopen(req)
     if http.getcode() != 200:
@@ -27,7 +26,6 @@ def get_cves_from_page(url, filename, version):
     soup = BeautifulSoup(html, 'html.parser')
     table = soup.find('table', attrs={'id': 'vulnslisttable'})
     result = [[td.text.encode("utf-8") for td in row.find_all("td")] for row in table.select("tr + tr")]
-    [result[x].append(version) for x in range(0, len(result), 2)]
     results = []
     for x in range(0, len(result), 2):
         results.append(result[x] + result[x + 1])
@@ -53,15 +51,15 @@ def write_headers(filename):
         wr = csv.writer(f)
         wr.writerow(headers)
 
-def get_cves(vendor, product, version):
+def get_cves(vendor, product, version, filename):
     print('[*] Retrieving cves from version {}'.format(version))
-    filename = "{name}.csv".format(name = vendor + product + version)
+    filename = filename + ".csv"
     write_headers(filename)
     links = get_links(url.format(vid = vendor, pid = product, verid = version))
     cves = []
-    print('[*] Writing cves to file {}'.format(filename))
+    print('[*] Writing cves to file {}.csv'.format(filename))
     for link in links:
-        cves.extend(get_cves_from_page(link, filename, version))
+        cves.extend(get_cves_from_page(link, filename))
     return cves
 
 #------------------------------------------------------------------------------
@@ -70,75 +68,30 @@ def get_cves(vendor, product, version):
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--vendor',
-                    help="Vendor (required)",
+                    help="Vendor id (required)",
                     action='store',
                     required=True,
                     dest='vendor')
 parser.add_argument('--product',
-                    help='Product (required)',
+                    help='Product id (required)',
                     action='store',
                     required=True,
                     dest='product')
 parser.add_argument('--version',
-                    help='Version (required)',
+                    help='Version id (required)',
                     action='store',
                     required=True,
                     dest='version')
-parser.add_argument('--upgrade',
-                    help="Newer version of product (required)",
+parser.add_argument('--csv',
+                    help='name of the csv to write the CVEs (required)',
                     action='store',
                     required=True,
-                    dest='upgrade')
-parser.add_argument('--excel',
-                    help='Nam of the excel file (required)',
-                    action='store',
-                    required=True,
-                    dest='excel')
+                    metavar='<output-filename>',
+                    dest='csv')
 
 args = parser.parse_args()
 
 #------------------------------------------------------------------------------
 # Retrieving data from website
 #------------------------------------------------------------------------------
-old = get_cves(args.vendor, args.product, args.version)
-upgrade = get_cves(args.vendor, args.product, args.upgrade)
-
-#------------------------------------------------------------------------------
-# Data processing
-#------------------------------------------------------------------------------
-print('[*] Processing data...')
-unpatched = []
-patched = []
-
-for cve in old:
-    if cve not in upgrade:
-        patched.append(cve)
-    elif cve in upgrade:
-        unpatched.append(cve)
-
-for cve in upgrade:
-    if cve not in unpatched:
-        unpatched.append(cve)
-
-#------------------------------------------------------------------------------
-# Save in excel
-#------------------------------------------------------------------------------
-
-print('[*] Saving in excel...')
-excelfile = pandas.ExcelWriter('{}.xlsx'.format(args.excel))
-
-dfold = pandas.read_csv('{}.csv'.format(args.vendor + args.product + args.version))
-dfnew = pandas.read_csv('{}.csv'.format(args.vendor + args.product + args.upgrade))
-
-dfold.to_excel(excelfile, sheet_name='CVEs old version')
-dfnew.to_excel(excelfile, sheet_name='CVEs new version')
-
-dfunpat = pandas.DataFrame(unpatched, columns=headers)
-dfpat = pandas.DataFrame(patched, columns=headers)
-
-dfunpat.to_excel(excelfile, sheet_name='Unpatched CVEs')
-dfpat.to_excel(excelfile, sheet_name='Patched CVEs')
-
-print('[*] Saving file {}.xlsx'.format(args.excel))
-excelfile.save()
-
+get_cves(args.vendor, args.product, args.version, args.csv)
